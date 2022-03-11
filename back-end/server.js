02e6944a99bb7bda364384fd48934fdb37cb7a9a
RohanPursuit <rohansterling@pursuit.org>
2022-03-11 08:46:37 -0500

    add middleware function for socket.io authentication, add get route / and /ALL route

--- back-end/server.js
+++ back-end/server.js
@@ -21,7 +21,27 @@ const io = new Server(httpServer, {
 // MIDDLEWARE
 app.use(cors())
 app.use(express.json())
+io.use((socket, next)=> {
+    const test = false
+    if(test){
+        next()
+    }else {
+        next(new Error("No Access"))
+    }
+}).on("connection", (socket) => {
+    console.log(socket)
+})
 
+//ROUTES
+app.get("/", (req, res)=> {
+    console.log("GET /")
+    res.send("Hello World")
+})
+
+app.get("*", (req, res)=> {
+    console.log("GET anything else")
+    res.send("There is nothing here")
+})
 
 // LISTEN
 const PORT = process.env.PORT
