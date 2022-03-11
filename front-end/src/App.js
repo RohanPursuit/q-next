84e001eea2a2b413dfdc73bb6312a58d4af38fd7
RohanPursuit <rohansterling@pursuit.org>
2022-03-11 09:07:01 -0500

    add dependencies Routes and Route from react-router-dom, import SignIn component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom"
+import SignIn from './Components/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<SignIn/>}/>
+        <Route path="/player" element={<h1>Q-NEXT Player</h1>}/>
+        <Route path="/room/:roomId" element={<h1>Q-NEXT Request</h1>}/>
+      </Routes>
     </div>
   );
 }
