917f4e5728e88853ac5198a3d9d17a278fa43ad4
RohanPursuit <rohansterling@pursuit.org>
2022-03-11 14:43:46 -0500

    add dependencies useState, useEffect, useCallback from react, dependence axois, add connects to server when component mounts using startConnect callback function, sending id of room to join on io.handleshake.query.id

--- front-end/src/Pages/Request.js
+++ front-end/src/Pages/Request.js
@@ -1,7 +1,40 @@
+import io from "socket.io-client"
+import {useEffect, useCallback} from "react"
+import axios from "axios"
+import { useParams } from "react-router-dom"
+
+const {REACT_APP_PORT: PORT, REACT_APP_API_URL: URL} = process.env
+
 const Request = () => {
+    const {id} = useParams()
+    const socket = useCallback(() => io(URL, {
+        query: {
+            id: id,
+        }
+    }))
+    
+    const startConnect = useCallback(() => {
+            socket().on("connect", () => {
+                console.log("connected")
+            }).on("private-message", (message) => {
+                axios.get(URL, {params: {id: message}})
+                .then((response) => {
+                    const data = response.data
+                    console.log(data)
+                })
+            })
+        }
+    )
+
+
+    useEffect(()=> {
+        startConnect()
+    }, [])
+
     return (
         <div className="PlayerPage">
             <h1>Q-NEXT Request</h1>
+            <h2>Room: {id}</h2>
         </div>
     )
 }
