21945e51918024e9cba2a3891f99db9febfaff81
RohanPursuit <rohansterling@pursuit.org>
2022-03-11 08:18:24 -0500

    add dependencies (fs, path, ytdl-core as ytdl, ytsr, createServer from http, Server from socket.io), configure Server and createServer, httpServer listening to PORT

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,22 @@
 //DEPENDENCIES
 const express = require("express")
 const cors = require("cors")
+const fs = require("fs")
+const path = require("path")
+const ytdl = require("ytdl-core")
+const ytsr = require("ytsr")
+const {createServer} = require("http")
+const {Server} = require("socket.io")
 
 // CONFIGURATION
 require("dotenv").config()
 const app = express()
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors:{
+        origin: []
+    }
+});
 
 // MIDDLEWARE
 app.use(cors())
@@ -13,6 +25,6 @@ app.use(express.json())
 
 // LISTEN
 const PORT = process.env.PORT
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`🔊Listening to music on port ${PORT}🔊`)
 })
