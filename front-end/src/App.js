974a257e80406c75d1e9b105604b49a620df4858
RohanPursuit <rohansterling@pursuit.org>
2022-03-11 09:16:28 -0500

    import Player and Request components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import {Routes, Route} from "react-router-dom"
 import SignIn from './Components/SignIn';
+import Player from './Pages/Player';
+import Request from './Pages/Request';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<SignIn/>}/>
-        <Route path="/player" element={<h1>Q-NEXT Player</h1>}/>
-        <Route path="/room/:roomId" element={<h1>Q-NEXT Request</h1>}/>
+        <Route path="/player" element={<Player/>}/>
+        <Route path="/room/:roomId" element={<Request/>}/>
       </Routes>
     </div>
   );
