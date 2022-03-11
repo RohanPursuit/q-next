2b1791e99f9a6497e912b78ea605ec29b1569e2f
RohanPursuit <rohansterling@pursuit.org>
2022-03-11 15:23:03 -0500

    import react-qr-code, using qr component in return

--- front-end/src/Pages/Player.js
+++ front-end/src/Pages/Player.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client"
 import {useState, useEffect, useCallback} from "react"
 import axios from "axios"
+import QRCode from "react-qr-code"
 
 const {REACT_APP_PORT: PORT, REACT_APP_API_URL: URL} = process.env
 
@@ -16,6 +17,7 @@ const Player = () => {
                 axios.get(URL, {params: {id: message}})
                 .then((response) => {
                     const data = response.data
+                    console.log(data)
                 })
             })
         }
@@ -30,6 +32,7 @@ const Player = () => {
         <div className="PlayerPage">
             <h1>Q-NEXT Player</h1>
             <h2>Room: {roomId}</h2>
+            <QRCode value={"http://192.168.1.159:3000/rooms/"+roomId} />
         </div>
     )
 }
