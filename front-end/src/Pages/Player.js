1329348e26a69a3769f9be0f26e8de0c2b7a8600
RohanPursuit <rohansterling@pursuit.org>
2022-03-11 09:41:35 -0500

    import socket.io-client as io, connecting to socket io backend

--- front-end/src/Pages/Player.js
+++ front-end/src/Pages/Player.js
@@ -1,4 +1,12 @@
+import io from "socket.io-client"
+
+const {REACT_APP_PORT: PORT, REACT_APP_API_URL: URL} = process.env
+
 const Player = () => {
+    const socket = io(URL)
+    socket.on("connect", () => {
+        console.log("connected")
+    })
     return (
         <div className="PlayerPage">
             <h1>Q-NEXT Player</h1>
