8da9f1938424b8243594198e5d040864e36cbf70
RohanPursuit <rohansterling@pursuit.org>
2022-03-11 21:34:42 -0500

    client can send and retrieve requests from server, player can retrieve request form server.

--- back-end/server.js
+++ back-end/server.js
@@ -18,6 +18,8 @@ const io = new Server(httpServer, {
     }
 });
 
+const playlist = {}
+
 // MIDDLEWARE
 app.use(cors())
 app.use(express.json())
@@ -36,6 +38,24 @@ io.use( async (socket, next)=> {
     }
 }).on("connection", (socket) => {
     io.to(socket.id).emit("private-message", socket.id)
+
+    socket.on("get-playlist", (id) => {
+        console.log("get-playlist ")
+        if(!playlist[id]){
+            socket.emit("get-playlist", [])
+        } else {
+            socket.emit("get-playlist", playlist[id])
+        }
+    })
+
+
+    socket.on("send-playlist", ({room, songs}) => {
+        console.log("send-playlist ", room)
+        playlist[room] = songs
+        socket.broadcast.emit("get-playlist", playlist[room])
+    })
+
+    //On Disconnect
     socket.on("disconnect", () => {
         //on disconnect find song for that room and delete
         console.log(`User ${socket.id} disconnected`)
@@ -49,6 +69,44 @@ app.get("/", async (req, res)=> {
     //if client connected stream file
     if(connectedSocket.has(req.query.id)){
         res.send("Hello World")
+
+    }else {
+        res.send("Not Connected")
+    }
+})
+
+app.post("/", async (req, res) => {
+    console.log("Post /")
+    const connectedSocket = await io.sockets.allSockets()
+    //if client connected stream file
+    if(connectedSocket.has(req.body.id)){
+        res.send("Hello World")
+
+    }else {
+        res.send("Not Connected")
+    }
+
+})
+
+app.post("/search", async (req, res) => {
+    console.log("Post /search")
+    const connectedSocket = await io.sockets.allSockets()
+    //if client connected stream file
+    console.log(req.body)
+    if(connectedSocket.has(req.body.id)){
+        const {q} = req.body
+        // console.log(q)
+        // const search = (await ytsr(q, {limit: 10})).items
+        // console.log(search)
+        // res.status(200).json({success: true, payload: search})
+        const filters1 = await ytsr.getFilters(q);
+        const filter1 = filters1.get('Type').get('Video');
+        const options = {
+            limit: 10,
+        }
+        const searchResults = (await ytsr(filter1.url, options)).items
+        // console.log(searchResults)
+        res.status(200).json({success: true, payload: searchResults})
 
     }else {
         res.send("Not Connected")
